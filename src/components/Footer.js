ed3454d6100c587010c118620ff80a6084708a36
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-09-13 21:43:30 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Inspiration from Soumyajit </h3>
+          <h3>Inspiration from S.B </h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Abrham Abebaw</h3>
